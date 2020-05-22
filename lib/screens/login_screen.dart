d13f1016742a2e0973e8c65e488ade3d367c2535
shreesh <shreesharya20@gmail.com>
2020-05-22 19:42:12 +0530

    registration screen designing

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,7 +19,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
