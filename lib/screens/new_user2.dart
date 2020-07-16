a6e7ab70622778811dcee113058e565481c44f51
shreesh <shreesharya20@gmail.com>
2020-07-16 23:52:04 +0530

    new user 2 page work in progress

---
+++ lib/screens/new_user2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewUser2 extends StatefulWidget {
+  static const String id='new_user2';
+  @override
+  _NewUser2State createState() => _NewUser2State();
+}
+
+class _NewUser2State extends State<NewUser2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
